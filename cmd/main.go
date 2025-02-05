e983ef956fa8756144c92058f0d86bbbcb766388
Iman-ws <230644@astanait.edu.kz>
2025-02-06 01:42:42 +0500

    Add initial folder structure and simple main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+// main is the entry point of our Go application.
+// It will eventually run the web server.
 func main() {
 	fmt.Println("Hello from PeriFyGo!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Simple test handler
+		w.Write([]byte("PeriFyGo says hi!"))
+	})
+
+	// Let's run on port 8080
+	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module PeriFyGo
 
 go 1.23.4
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.mongodb.org/mongo-driver v1.17.2 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/time v0.10.0 // indirect
+)
